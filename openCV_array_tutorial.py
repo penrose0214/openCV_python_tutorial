69d480b6976d80a498debd4b1f7f302277b36d12
penrose0214 <penrose0214@gmail.com>
2022-08-10 23:26:32 +0900

    array tutorial for python-openCV

---
+++ openCV_array_tutorial.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+array1 = np.array([1, 2, 3])    #defined 1 X 3 array with numpy library
+array2 = np.array([[1, 2], [3, 4]]) #defined 2 X 2 array 
+array3 = np.array([[[1, 2], [3, 4]], [[5, 6], [7, 8]] ])
+
+print(array1[-1])
+print(array2[0][1])
+print(array3[0][1][2])
