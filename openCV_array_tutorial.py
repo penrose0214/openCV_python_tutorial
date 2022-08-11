45f8c0af95699e6af170170c065188eea1ba526a
penrose0214 <penrose0214@gmail.com>
2022-08-11 15:36:33 +0900

    fourth commit

--- openCV_array_tutorial.py
+++ openCV_array_tutorial.py
@@ -11,6 +11,8 @@ print(array3[0][1][1])
 array4 = np.array([[1, 2, 3, 4, 5], 
                    [6, 7, 8, 9, 10], 
                    [11, 12, 13, 14, 15],
-                   [16, 17, 18, 19, 20]])
+                   [16, 17, 18, 19, 20]]) # 4X5 matrix 
 
-print(array4[1:3])
+print(array4[1:3]) #numpy array -> *[start:end:step]
+
+
