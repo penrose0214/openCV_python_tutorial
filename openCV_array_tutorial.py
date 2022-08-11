237c2395ac8e7d405d5cf37a8c58e2fa7ea750ee
penrose0214 <penrose0214@gmail.com>
2022-08-11 16:22:52 +0900

    added image input & output

--- openCV_array_tutorial.py
+++ openCV_array_tutorial.py
@@ -1,4 +1,5 @@
 import numpy as np
+import cv2 as cv
 
 array1 = np.array([1, 2, 3])    #defined 1 X 3 array with numpy library
 array2 = np.array([[1, 2], [3, 4]]) #defined 2 X 2 array 
@@ -25,3 +26,16 @@ x, y, w, h = 100, 100, 300, 300
 roi = array5[x:x+w, y:y+h]
 print(array5.shape) #몇by몇 행렬인지 print하기
 print(roi.shape) #몇by몇 행렬인지 print
+
+
+#image input using python-openCV library
+src = cv.imread("github.png", cv.IMREAD_GRAYSCALE)
+print(src.ndim, src.shape, src.dtype) #print out github.png image details
+
+#image output using python-openCV library 
+cv.namedWindow("src", flags=cv.WINDOW_FREERATIO)
+cv.resizeWindow("src", 400, 400)
+cv.imshow("src", src)
+cv.waitKey(0)
+cv.destroyWindow("src")
+
