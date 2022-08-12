d828a8f976b2cadd74bbb4e9845bb66e589011fc
penrose0214 <penrose0214@gmail.com>
2022-08-12 21:32:13 +0900

    another commit

--- openCV_callback_function.py
+++ openCV_callback_function.py
@@ -3,15 +3,22 @@ import numpy as np
 
 def mouse_event(event, x, y, flags, param):
     global radius
-    if event == cv.circle(param, (x, y), radius, (255, 0, 0), 2):
+    if event == cv.EVENT_FLAG_LBUTTON:
+        cv.circle(param, (x, y), radius, (0, 255, 0), 2)
         cv.imshow("draw", src)
         
-    elif event == cv. EVENt_MOUSEWHEEL:
+    elif event == cv.EVENT_FLAG_RBUTTON:
         if flags > 0:
             radius += 1
         elif radius > 1:
             radius -= 1
 
 radius = 3
-src = np.full((500, 500, 3), 255, dtype=np.uint8)
+src = np.full((500, 490, 3), 255, dtype=np.uint8)
+
+cv.imshow("draw", src)
+cv.setMouseCallback("draw", mouse_event, src)
+cv.waitKey()
+cv.destroyAllWindows()
+
      
