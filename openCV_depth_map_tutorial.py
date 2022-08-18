ae87b7c6b397a6fb7a895712691594d34834230a
penrose0214 <penrose0214@gmail.com>
2022-08-18 16:33:05 +0900

    thrid commit

--- openCV_depth_map_tutorial.py
+++ openCV_depth_map_tutorial.py
@@ -2,11 +2,13 @@ import numpy as np
 import cv2 as cv
 import matplotlib.pyplot as plt
 
-L_img = cv.imread("Left_image.png")
-R_img = cv.imread("Right_image.png")
+L_img = cv.imread('Left_image.png', 0)
+R_img = cv.imread('Right_image.png', 0)
 
 stereo = cv.StereoBM_create(numDisparities = 16, blockSize = 15)
 disparity = stereo.compute(L_img, R_img)
 
 plt.imshow(disparity, cmap='gray') #cmap = 'gray' refers to gray-scale image
 plt.show()
+
+print(disparity)
