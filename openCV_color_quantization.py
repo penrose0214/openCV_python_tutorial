6812e119365afebcbb63a45b6f6098d29c0a8192
penrose0214 <penrose0214@gmail.com>
2022-08-22 16:38:25 +0900

    additional commit

--- openCV_color_quantization.py
+++ openCV_color_quantization.py
@@ -4,13 +4,20 @@ import cv2 as cv
 '''
 This code is for color-quantization of certain input image file;
 color quantization is necessary for saving space in given memory
+
+pros:
+noise-resistant
+
+cons:
+instance-based learning -> slow process
+-> requires reduction in demensionality in order to speed up
 '''
 src = cv.imread("tomato.jpg")
 src_reshaped = src.reshape((-1, 3)) # -1 in reshape(-1, 3) means that original src matrix form will be fit into n X 3 matrix where number n will be formatted accordingly
 src_reshaped = np.float32(src_reshaped)
 #above 2 lines can be replaced with src.reshaped = src.reshape((-1, 3)).astype(np.float32)
 
-criteria = (cv.TERM_CRITERIA_EPS + cv.TERM_CRITERIA_MAX_ITER, 10, 1.0)
+criteria = (cv.TERM_CRITERIA_EPS + cv.TERM_CRITERIA_MAX_ITER, 10, 0.000001)
 K = 5 # smaller K-value --> simpler colors 
 retval, bestL, center = cv.kmeans(src_reshaped, K, None, criteria, 10, cv.KMEANS_RANDOM_CENTERS)
 
